dcb23d4de0420d930d60fc3d26f40b4927ae6a31
RobertHan96 <yshan4329@gmail.com>
2020-05-06 19:38:44 +0900

    Initial commit

---
+++ app/src/main/java/com/nexon/editmyinfopractice_20200506/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.nexon.editmyinfopractice_20200506
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
