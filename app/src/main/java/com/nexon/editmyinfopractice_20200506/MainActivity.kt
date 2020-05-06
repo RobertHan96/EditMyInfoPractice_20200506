c715475afa1e460ddc343130263bd5c1f4f298f1
RobertHan96 <yshan4329@gmail.com>
2020-05-06 19:58:01 +0900

    3th commit : 스피너 선택지값을 가져오는 코드 추가

--- app/src/main/java/com/nexon/editmyinfopractice_20200506/MainActivity.kt
+++ app/src/main/java/com/nexon/editmyinfopractice_20200506/MainActivity.kt
@@ -2,6 +2,8 @@ package com.nexon.editmyinfopractice_20200506
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        okBtn.setOnClickListener {
+            val selectedJob =  jobSpinner.selectedItem as String
+            Log.d("Selected Job ", selectedJob)
+
+            val selectedPostion = jobSpinner.selectedItemPosition
+            Log.d("Selected Position ", "${selectedPostion}")
+
+        }
 
     }
 
