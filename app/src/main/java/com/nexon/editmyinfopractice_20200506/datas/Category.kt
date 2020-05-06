22b294ec174c1923ad2ba405178434410c6ecb17
RobertHan96 <yshan4329@gmail.com>
2020-05-06 21:12:06 +0900

    8th commit : 사용자, 카테고리 데이터 클래스 생성

---
+++ app/src/main/java/com/nexon/editmyinfopractice_20200506/datas/Category.kt
@@ -0,0 +1,7 @@
+package com.nexon.editmyinfopractice_20200506.datas
+
+class Category {
+    var id = 0
+    var title = ""
+    var color = ""
+}
---
+++ app/src/main/java/com/nexon/editmyinfopractice_20200506/datas/User.kt
@@ -0,0 +1,14 @@
+package com.nexon.editmyinfopractice_20200506.datas
+
+import java.util.*
+
+class User {
+    var id = 0
+    var logindId = ""
+    var name = ""
+    var phoneNum = ""
+    var memo = ""
+    var createdAt = Calendar.getInstance()
+    var category = Category()
+
+}
