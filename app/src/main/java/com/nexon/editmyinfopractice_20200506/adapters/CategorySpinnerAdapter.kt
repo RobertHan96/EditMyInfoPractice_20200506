c0d4fbe1fa6c2f5580720319ca1c27eecc999da3
RobertHan96 <yshan4329@gmail.com>
2020-05-07 21:04:26 +0900

    20th commit : 스피너에 실제 데이터 값 반영, 스피너 부분 완성 - String 값을 가지고 색상코드로 바꿔서 적용하는 코드 추가

--- app/src/main/java/com/nexon/editmyinfopractice_20200506/adapters/CategorySpinnerAdapter.kt
+++ app/src/main/java/com/nexon/editmyinfopractice_20200506/adapters/CategorySpinnerAdapter.kt
@@ -1,6 +1,7 @@
 package com.nexon.editmyinfopractice_20200506.adapters
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,11 +21,14 @@ class CategorySpinnerAdapter(val mContext : Context, val resId : Int, val mList 
             tempRow = inf.inflate(R.layout.category_list_item, null)
         }
         val row = tempRow!!
+        val colorImg = row.findViewById<ImageView>(R.id.colorImg)
+        val categoryTitleText = row.findViewById<TextView>(R.id.categoryTitleText)
+        val data = mList.get(position)
 
-
-
+//      스트링값을 실제 색상코드로 변환해서 백그라운드 색상으로 적용하는 코드  
+        colorImg.setBackgroundColor(Color.parseColor(data.color))
+        categoryTitleText.text = data.title
         return  row
-
     }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
@@ -36,10 +40,12 @@ class CategorySpinnerAdapter(val mContext : Context, val resId : Int, val mList 
 
         }
         val row = tempRow!!
-        val logoImg = row.findViewById<ImageView>(R.id.logoImg)
-        val nameText = row.findViewById<TextView>(R.id.nameText)
+        val colorImg = row.findViewById<ImageView>(R.id.colorImg)
+        val categoryTitleText = row.findViewById<TextView>(R.id.categoryTitleText)
         val data = mList.get(position)
-
+        
+        colorImg.setBackgroundColor(Color.parseColor(data.color))
+        categoryTitleText.text = data.title
         return  row
     }
 }
