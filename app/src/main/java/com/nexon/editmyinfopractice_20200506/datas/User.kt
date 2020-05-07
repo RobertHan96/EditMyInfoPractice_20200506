350051620d06bf8a02d543bd00f6bb3875969f93
RobertHan96 <yshan4329@gmail.com>
2020-05-07 19:54:44 +0900

    14th commit : 서버상에 저장된 가입일시를 Caleander 형태로 저장

--- app/src/main/java/com/nexon/editmyinfopractice_20200506/datas/User.kt
+++ app/src/main/java/com/nexon/editmyinfopractice_20200506/datas/User.kt
@@ -1,6 +1,7 @@
 package com.nexon.editmyinfopractice_20200506.datas
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
 import java.util.*
 
 class User {
@@ -13,9 +14,12 @@ class User {
             parsedUser.name = json.getString("name")
             parsedUser.phoneNum = json.getString("phone")
             parsedUser.memo = json.getString("memo")
-
             parsedUser.category = Category.getCategoryFromJson(json.getJSONObject("category"))
 
+            val createdAtStr = json.getString("created_at")
+            val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+            parsedUser.createdAt.time = sdf.parse(createdAtStr)
+//          가일일시의 값을 Date타입으로 변환한 객체 삽입,
 //            parsedUser.createdAt.set()
 
             return parsedUser
